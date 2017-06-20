9eb8f57125706fce4286df8d8a9d2b0e9618d5a0
victormier <victormier@gmail.com>
2017-06-20 17:22:06 +0200

    Add user validations

--- api/models/user.rb
+++ api/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
 
   has_many :comments
   has_many :posts
+
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_presence_of :password_digest
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-RSpec.describe User do
+RSpec.describe User, type: :model do
   subject {
     described_class.new(first_name: "John",
                         last_name: "Doe",
                         email: "johndoe@example.com",
                         password: "password")
   }
+
+  context "validations" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:password_digest) }
+    it { is_expected.to have_secure_password }
+  end
 
   it "allows creation of a user" do
     expect(subject.save).to be true
@@ -15,4 +22,5 @@ RSpec.describe User do
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
+
 end
