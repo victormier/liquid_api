85f192bd9314809e85f7adf94cef9f0c4ba857dc
victormier <victormier@gmail.com>
2017-07-27 17:50:27 +0200

    Use config host for generating email url

--- spec/routes/emails_spec.rb
+++ spec/routes/emails_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "/emails" do
         expect do
           LiquidApi.sendmail("/emails/users/email_confirmation", user.id)
         end.to change{ Mail::TestMailer.deliveries.length }.by(1)
-        confirmation_token_url = "https://helloliquid.com/users/confirm_email?confirmation_token=#{user.confirmation_token}"
+        confirmation_token_url = "https://localhost:3000/users/confirm_email?confirmation_token=#{user.confirmation_token}"
 
         expect(Mail::TestMailer.deliveries.last.to).to include user.email
         expect(Mail::TestMailer.deliveries.last.body).to include confirmation_token_url
