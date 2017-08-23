b4ed75f021e53162b5370b1f6b7981f8c85aa421
victormier <victormier@gmail.com>
2017-08-23 13:06:06 +0200

    Add service for updating saltedge providers

---
+++ api/services/update_saltedge_providers.rb
@@ -0,0 +1,40 @@
+module Services
+  class UpdateSaltedgeProviders
+    STOREABLE_ATTRIBUTES = %w(saltedge_id saltedge_data name status mode automatic_fetch country_code)
+
+    def initialize(page = nil)
+      @saltedge_client = SaltedgeClient.new
+    end
+
+    def call
+      providers_data, next_id = retrieve_providers
+      while next_id do
+        store_providers(providers_data)
+        providers_data, next_id = retrieve_providers(next_id)
+      end
+    end
+
+    private
+
+    def retrieve_providers(from_id = nil)
+      params = {
+        include_provider_fields: true
+      }
+      params[:from_id] = from_id unless from_id.nil?
+      response = @saltedge_client.request(:get, "/providers", params)
+      parsed_response = JSON.parse(response.body)
+
+      [parsed_response["data"], parsed_response["meta"]["next_id"]]
+    end
+
+    def store_providers(providers_hash)
+      providers_hash.each do |provider_data|
+        saltedge_provider = SaltedgeProvider.find_or_initialize_by(saltedge_id: provider_data["id"])
+        attrs = provider_data.select {|k,v| STOREABLE_ATTRIBUTES.include?(k) }
+        saltedge_provider.assign_attributes(attrs)
+        saltedge_provider.saltedge_data = provider_data
+        saltedge_provider.save
+      end
+    end
+  end
+end
