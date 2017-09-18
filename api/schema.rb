35656386ce1a29af250ebd97fe973de87cc28b50
victormier <victormier@gmail.com>
2017-09-18 17:20:11 +0200

    Add user field to graphql schema

--- api/schema.rb
+++ api/schema.rb
@@ -8,6 +8,11 @@ QueryRoot = GraphQL::ObjectType.define do
     resolve -> (obj, args, ctx) { Post.find(args["id"]) }
   end
 
+  field :user do
+    type UserType
+    resolve -> (obj, args, ctx) { ctx[:current_user] }
+  end
+
   field :all_posts do
     type types[!PostType]
     resolve -> (obj, args, ctx) { Post.all }
