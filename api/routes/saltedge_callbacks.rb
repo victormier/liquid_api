410afcc4c714ec5b2398a9045d2d937f3f6be65c
victormier <victormier@gmail.com>
2017-09-18 18:13:34 +0200

    Dont select extra accounts for a login

--- api/routes/saltedge_callbacks.rb
+++ api/routes/saltedge_callbacks.rb
@@ -5,7 +5,7 @@ LiquidApi.route("saltedge_callbacks") do |r|
     saltedge_login = SaltedgeLogin.find_by(saltedge_id: login_id)
     if saltedge_login
       Services::UpdateSaltedgeLogin.new(saltedge_login).call
-      unless saltedge_login.user.default_account
+      unless saltedge_login.saltedge_accounts.any? || user.default_account
         service = Services::SelectSaltedgeAccount.new(saltedge_login)
         service.call
         if service.saltedge_account
