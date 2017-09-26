2e3f56d8489d706be682e93ae056e8b759dd837e
victormier <victormier@gmail.com>
2017-09-26 10:45:31 +0200

    Delegate some methods from mirror to saltedge transaction

--- api/models/mirror_transaction.rb
+++ api/models/mirror_transaction.rb
@@ -3,4 +3,6 @@ require 'api/models/transaction'
 # Represent a mirror of a real transaction
 class MirrorTransaction < Transaction
   belongs_to :saltedge_transaction
+
+  delegate :description, :category, to: :saltedge_transaction
 end
