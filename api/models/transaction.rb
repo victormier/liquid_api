b0278624bd23f37bd23f5cafaab2b0987d6c8184
victormier <victormier@gmail.com>
2017-10-02 17:38:49 +0200

    Add scopes to transaction

--- api/models/transaction.rb
+++ api/models/transaction.rb
@@ -4,4 +4,8 @@ class Transaction < ActiveRecord::Base
   scope :newest_first, -> { order('made_on desc, created_at desc') }
   scope :virtual, -> { where(type: 'VirtualTransaction') }
   scope :mirror, -> { where(type: 'MirrorTransaction') }
+  scope :credit, -> { where("amount < 0.0") }
+  scope :debit, -> { where("amount > 0.0") }
+  scope :from_date, -> (date) { where("made_on >= ?", date) }
+  scope :to_date, -> (date) { where("made_on <= ?", date) }
 end
