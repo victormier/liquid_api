0e4fc79d55ae704970e9908e1cba8fca46724ac8
victormier <victormier@gmail.com>
2017-11-01 20:43:39 +0100

    Use saltedge_id for sorting transactions

--- api/models/transaction.rb
+++ api/models/transaction.rb
@@ -1,13 +1,13 @@
 class Transaction < ActiveRecord::Base
   belongs_to :virtual_account
 
-  scope :newest_first, -> { order('made_on desc, created_at desc') }
-  scope :oldest_first, -> { order('made_on asc, created_at asc') }
-  scope :virtual, -> { where(type: 'VirtualTransaction') }
-  scope :mirror, -> { where(type: 'MirrorTransaction') }
-  scope :credit, -> { where("amount < 0.0") }
-  scope :debit, -> { where("amount > 0.0") }
-  scope :from_date, -> (date) { where("made_on >= ?", date) }
-  scope :to_date, -> (date) { where("made_on <= ?", date) }
-  scope :automatic, -> { where('rule_id IS NOT NULL') }
+  scope :newest_first, -> { order('transactions.made_on desc, transactions.saltedge_id desc') }
+  scope :oldest_first, -> { order('transactions.made_on asc, transactions.created_at asc') }
+  scope :virtual, -> { where("transactions.type" => 'VirtualTransaction') }
+  scope :mirror, -> { where("transactions.type" => 'MirrorTransaction') }
+  scope :credit, -> { where("transactions.amount < 0.0") }
+  scope :debit, -> { where("transactions.amount > 0.0") }
+  scope :from_date, -> (date) { where("transactions.made_on >= ?", date) }
+  scope :to_date, -> (date) { where("transactions.made_on <= ?", date) }
+  scope :automatic, -> { where('transactions.rule_id IS NOT NULL') }
 end
