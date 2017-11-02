5dd986aa4091eb992626eec106076843090b3433
victormier <victormier@gmail.com>
2017-11-02 17:51:50 +0100

    Fix saltedge account data not being stored properly

--- api/services/update_saltedge_account.rb
+++ api/services/update_saltedge_account.rb
@@ -10,13 +10,15 @@ module Services
         login_id: @saltedge_account.saltedge_login.saltedge_id
       })
       data = JSON.parse(response.body)
-      account_data = data["data"].find { |a| a["id"] == @saltedge_account.saltedge_id }
+      account_data = data["data"].find { |a| a["id"].to_s == saltedge_account.saltedge_id }
 
       if account_data
         @saltedge_account.update_attributes({
           saltedge_data: account_data
         })
       end
+
+      @saltedge_account.virtual_account.try(:compute_balance!)
     end
   end
 end
