5a48e36b6aca6ae5e77885c506b8bc2061030a12
victormier <victormier@gmail.com>
2017-11-08 18:20:36 +0100

    Set hosts in env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
 LiquidApi.configure do |config|
-  config.default_client_host = "app.helloliquid.com"
-  config.default_api_host = "api.helloliquid.com"
+  config.default_client_host = ENV['DEFAULT_CLIENT_HOST']
+  config.default_api_host = ENV['DEFAULT_API_HOST']
 end
