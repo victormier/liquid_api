fa22412716e7f665423ded89389cd0ebf5143d79
victormier <victormier@gmail.com>
2017-11-09 17:13:15 +0100

    Add option to pass data in update saltedge account

--- api/services/update_saltedge_account.rb
+++ api/services/update_saltedge_account.rb
@@ -1,20 +1,23 @@
 module Services
   class UpdateSaltedgeAccount
-    def initialize(saltedge_account)
+    def initialize(saltedge_account, account_data = {})
       @saltedge_account = saltedge_account
       @saltedge_client = SaltedgeClient.new
+      @account_data = account_data
     end
 
     def call
-      response = @saltedge_client.request(:get, "/accounts", {
-        login_id: @saltedge_account.saltedge_login.saltedge_id
-      })
-      data = JSON.parse(response.body)
-      account_data = data["data"].find { |a| a["id"].to_s == @saltedge_account.saltedge_id }
+      unless @account_data.present?
+        response = @saltedge_client.request(:get, "/accounts", {
+          login_id: @saltedge_account.saltedge_login.saltedge_id
+        })
+        data = JSON.parse(response.body)
+        @account_data = data["data"].find { |a| a["id"].to_s == @saltedge_account.saltedge_id }
+      end
 
-      if account_data
+      if @account_data.present?
         @saltedge_account.update_attributes({
-          saltedge_data: account_data
+          saltedge_data: @account_data
         })
       end
 
--- spec/services/update_saltedge_account_spec.rb
+++ spec/services/update_saltedge_account_spec.rb
@@ -53,34 +53,11 @@ RSpec.describe Services::UpdateSaltedgeAccount do
       }.to change(LoadTransactionsWorker.jobs, :size).by(0)
     end
   end
-  #
-  # it "creates a saltedge account record for each account found" do
-  #   service = Services::UpdateSaltedgeAccount.new(saltedge_login)
-  #   expect { service.call }.to change{ user.saltedge_accounts.count }.by(2)
-  #   last_saltedge_account = user.saltedge_accounts.last
-  #   expect(last_saltedge_account.saltedge_data).to_not be_empty
-  # end
-  #
-  # it "doesn't create new records if account exists" do
-  #   service = Services::UpdateSaltedgeAccount.new(saltedge_login)
-  #   expect { service.call }.to change{ user.saltedge_accounts.count }.by(2)
-  #   new_service = Services::UpdateSaltedgeAccount.new(saltedge_login)
-  #   expect { new_service.call }.to change{ user.saltedge_accounts.count }.by(0)
-  # end
-  #
-  # it "only selects whitelisted nature accounts" do
-  #   response_without_whitelisted_nature = begin
-  #     account_data = JSON.parse(saltedge_accounts_list_response)
-  #     account_data["data"].select do |a|
-  #       !SaltedgeAccount::ACCOUNT_NATURE_WHITELIST.include?(a["nature"])
-  #     end
-  #   end
-  #   stub_request(:get, "https://www.saltedge.com/api/v3/accounts").
-  #     to_return(
-  #       body: { "data" => response_without_whitelisted_nature}.to_json,
-  #       headers: { 'Content-Type' => 'application/json' }
-  #     )
-  #   service = Services::UpdateSaltedgeAccount.new(saltedge_login)
-  #   expect { service.call }.to change{ user.saltedge_accounts.count }.by(0)
-  # end
+
+  it "stores account_data paramater if it is passed" do
+    account_data = JSON.parse(saltedge_accounts_list_response)["data"][0]
+    account_data["balance"] = 1234.5
+    Services::UpdateSaltedgeAccount.new(saltedge_account, account_data).call
+    expect(saltedge_account.balance).to eq 1234.5
+  end
 end
