41ef0f6580b2000324f2a4749281aa0a3783eb8c
victormier <victormier@gmail.com>
2017-11-23 19:53:36 +0100

    Refactor load saltedge transactions service

--- api/services/load_saltedge_account_transactions.rb
+++ api/services/load_saltedge_account_transactions.rb
@@ -7,21 +7,25 @@ module Services
 
     def call
       Services::UpdateSaltedgeAccount.new(@saltedge_account, skip_load_transactions: true).call
-      from_id = @saltedge_account.saltedge_transactions.newest_first.last.try(:saltedge_id)
+      from_id = @saltedge_account.saltedge_transactions.saltedge_id_ordered.first.try(:saltedge_id)
 
       transactions_data, next_id = retrieve_transactions(from_id)
       while next_id do
-        transactions_data.each do |transaction_data|
-          StoreSaltedgeTransactionWorker.perform_async(@saltedge_account.id, transaction_data)
-        end
+        store_transactions_from_data(transactions_data, from_id)
         transactions_data, next_id = retrieve_transactions(next_id)
       end
-      transactions_data.each do |transaction_data|
-        StoreSaltedgeTransactionWorker.perform_async(@saltedge_account.id, transaction_data)
-      end
+      store_transactions_from_data(transactions_data, from_id)
     end
 
     private
+
+    def store_transactions_from_data(transactions_data, except_id = nil)
+      transactions_data.each do |transaction_data|
+        unless transaction_data["id"].to_s == except_id.to_s
+          StoreSaltedgeTransactionWorker.perform_async(@saltedge_account.id, transaction_data)
+        end
+      end
+    end
 
     def retrieve_transactions(from_id = nil)
       params = {
