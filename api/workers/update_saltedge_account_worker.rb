22d4c4803ed018741120fb55b905205492ece160
victormier <victormier@gmail.com>
2017-11-27 15:33:12 +0100

    Fix typo

--- api/workers/update_saltedge_account_worker.rb
+++ api/workers/update_saltedge_account_worker.rb
@@ -2,7 +2,7 @@ class UpdateSaltedgeAccountWorker
   include Sidekiq::Worker
 
   def perform(saltedge_account_id)
-    if saltedge_acocunt = SaltedgeAccount.find(saltedge_account_id)
+    if saltedge_account = SaltedgeAccount.find(saltedge_account_id)
       Services::UpdateSaltedgeAccount.new(saltedge_account).call
     end
   end
