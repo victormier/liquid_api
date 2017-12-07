f62323cf0f968fe6a654edfd9c72fbb1734bd2eb
victormier <victormier@gmail.com>
2017-12-07 14:07:13 +0100

    Add send email worker

--- api/services/send_email_confirmation_email.rb
+++ api/services/send_email_confirmation_email.rb
@@ -8,9 +8,6 @@ class SendEmailConfirmationEmail
     @user.confirmation_token = SecureRandom.hex(10)
     @user.confirmation_sent_at = Time.now.utc
     @user.save!
-    # To Do: Use a background job for sending emails
-    mail = LiquidApi.mail("/emails/users/email_confirmation", @user.id)
-    mail.reply_to 'noreply@helloliquid.com'
-    mail.deliver
+    SendEmailWorker.perform_async("/emails/users/email_confirmation", @user.id)
   end
 end
